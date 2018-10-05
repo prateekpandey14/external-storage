af80c03be9cb96c99f4342dc5f8a9bd3e390c071
prateekpandey14 <prateekpandey14@gmail.com>
2018-10-05 21:23:11 +0530

    refactor(ci): disable the snapshot test from ci temporarily
    The snapshot workflow is being changed to use the CAS templates.
    (openebs/maya #602 #527)
    
    Until it's updated to use the CAS templates way of creating the
    snapshots, disabling them from CI.
    
    Signed-off-by: prateekpandey14 <prateekpandey14@gmail.com>

--- openebs/ci/snapshot/snapshot_deploy.sh
+++ openebs/ci/snapshot/snapshot_deploy.sh
@@ -4,8 +4,8 @@ echo "*****************************Deploying Openebs***************************"
 CI_BRANCH="master"
 CI_TAG="ci"
 
-#Images from this repo are always tagged as ci 
-#The downloaded operator file will may contain a non-ci tag name 
+#Images from this repo are always tagged as ci
+#The downloaded operator file will may contain a non-ci tag name
 # depending on when and from where it is being downloaded. For ex:
 # - during the release time, the image tags can be versioned like 0.7.0-RC..
 # - from a branch, the image tags can be the branch names like v0.7.x-ci
@@ -76,60 +76,60 @@ for i in $(seq 1 100) ; do
     fi
 done
 
-echo "********************Creating volume snapshot*****************************"
-kubectl create -f  $DST_REPO/external-storage/openebs/ci/snapshot/snapshot.yaml
-kubectl logs --tail=20 -n openebs deployment/openebs-snapshot-operator -c snapshot-controller
-
-# Promote/restore snapshot as persistent volume
-sleep 30
-echo "*****************Promoting snapshot as new PVC***************************"
-kubectl create -f  $DST_REPO/external-storage/openebs/ci/snapshot/snapshot_claim.yaml
-kubectl logs --tail=20 -n openebs deployment/openebs-snapshot-operator -c snapshot-provisioner
-
-sleep 30
-# get clone replica pod IP to make a curl request to get hte clone status
-cloned_replica_ip=$(kubectl get pods -owide -l openebs.io/persistent-volume-claim=demo-snap-vol-claim --no-headers | grep -v ctrl | awk {'print $6'})
-echo "***************** checking clone status *********************************"
-for i in $(seq 1 5) ; do
-		clonestatus=`curl http://$cloned_replica_ip:9502/v1/replicas/1 | jq '.clonestatus' | tr -d '"'`
-		if [ "$clonestatus" == "completed" ]; then
-        break
-			else
-        echo "Clone process in not completed ${clonestatus}"
-        sleep 60
-    fi
-done
-
-# Clone is in Alpha state, and kind of flaky sometimes, comment this integration test below for time being,
-# util its stable in backend storage engine
-sleep 30
-echo "***************Creating busybox-clone application pod********************"
-kubectl create -f $DST_REPO/external-storage/openebs/ci/snapshot/busybox_clone.yaml
-
-kubectl get pods --all-namespaces
-kubectl get pvc --all-namespaces
-
-for i in $(seq 1 50) ; do
-    phase=$(kubectl get pods busybox-clone --output="jsonpath={.status.phase}")
-    if [ "$phase" == "Running" ]; then
-        break
-    else
-        echo "--------------busybox-clone pod is not ready yet-----------------"
-        kubectl describe pods busybox-clone
-		sleep 10
-    fi
-done
-
-kubectl get pods
-kubectl get pvc
-
-echo "*************Varifying data validity and Md5Sum Check********************"
-hash1=$(kubectl exec busybox -- md5sum /mnt/store1/date.txt | awk '{print $1}')
-hash2=$(kubectl exec busybox-clone -- md5sum /mnt/store2/date.txt | awk '{print $1}')
-echo "busybox hash: $hash1"
-echo "busybox-clone hash: $hash2"
-if [[ $hash1 == $hash2 ]]; then
-	 echo "Md5Sum Check: PASSED"
-else
-echo "Md5Sum Check: FAILED"; exit 1
-fi
+#echo "********************Creating volume snapshot*****************************"
+#kubectl create -f  $DST_REPO/external-storage/openebs/ci/snapshot/snapshot.yaml
+#kubectl logs --tail=20 -n openebs deployment/openebs-snapshot-operator -c snapshot-controller
+#
+## Promote/restore snapshot as persistent volume
+#sleep 30
+#echo "*****************Promoting snapshot as new PVC***************************"
+#kubectl create -f  $DST_REPO/external-storage/openebs/ci/snapshot/snapshot_claim.yaml
+#kubectl logs --tail=20 -n openebs deployment/openebs-snapshot-operator -c snapshot-provisioner
+#
+#sleep 30
+## get clone replica pod IP to make a curl request to get hte clone status
+#cloned_replica_ip=$(kubectl get pods -owide -l openebs.io/persistent-volume-claim=demo-snap-vol-claim --no-headers | grep -v ctrl | awk {'print $6'})
+#echo "***************** checking clone status *********************************"
+#for i in $(seq 1 5) ; do
+#		clonestatus=`curl http://$cloned_replica_ip:9502/v1/replicas/1 | jq '.clonestatus' | tr -d '"'`
+#		if [ "$clonestatus" == "completed" ]; then
+#        break
+#			else
+#        echo "Clone process in not completed ${clonestatus}"
+#        sleep 60
+#    fi
+#done
+#
+## Clone is in Alpha state, and kind of flaky sometimes, comment this integration test below for time being,
+## util its stable in backend storage engine
+#sleep 30
+#echo "***************Creating busybox-clone application pod********************"
+#kubectl create -f $DST_REPO/external-storage/openebs/ci/snapshot/busybox_clone.yaml
+#
+#kubectl get pods --all-namespaces
+#kubectl get pvc --all-namespaces
+#
+#for i in $(seq 1 50) ; do
+#    phase=$(kubectl get pods busybox-clone --output="jsonpath={.status.phase}")
+#    if [ "$phase" == "Running" ]; then
+#        break
+#    else
+#        echo "--------------busybox-clone pod is not ready yet-----------------"
+#        kubectl describe pods busybox-clone
+#		sleep 10
+#    fi
+#done
+#
+#kubectl get pods
+#kubectl get pvc
+#
+#echo "*************Varifying data validity and Md5Sum Check********************"
+#hash1=$(kubectl exec busybox -- md5sum /mnt/store1/date.txt | awk '{print $1}')
+#hash2=$(kubectl exec busybox-clone -- md5sum /mnt/store2/date.txt | awk '{print $1}')
+#echo "busybox hash: $hash1"
+#echo "busybox-clone hash: $hash2"
+#if [[ $hash1 == $hash2 ]]; then
+#	 echo "Md5Sum Check: PASSED"
+#else
+#echo "Md5Sum Check: FAILED"; exit 1
+#fi
