079abc4827ade8caff05cef67032f8e9c0beeeef
prateekpandey14 <prateek.pandey@openebs.io>
2020-04-10 19:02:35 +0530

    refact(arm): use single Dockerfile with base_image args
    Signed-off-by: prateekpandey14 <prateek.pandey@openebs.io>

--- openebs-build.sh
+++ openebs-build.sh
@@ -28,14 +28,6 @@ rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 cd $DST_REPO/external-storage/snapshot
 export REGISTRY="openebs/"
 export VERSION="ci"
-
-if [ "$TRAVIS_CPU_ARCH" == "amd64" ]; then
-  echo "Building snapshot-controller and snapshot-provisioner"
-  make container
-  rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-
-elif [ "$TRAVIS_CPU_ARCH" == "arm64" ]; then
-   echo "Building arm64 snapshot-controller and snapshot-provisioner"
-   make container.arm64
-  rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-fi
+echo "Building snapshot-controller and snapshot-provisioner"
+make container
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
